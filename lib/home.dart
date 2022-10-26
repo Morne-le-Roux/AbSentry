dec4854d07ad983c12f123ccc219a98a74abfb19
Morné le Roux <mornelerx@gmail.com>
2022-10-26 18:44:47 +0200

    Added basic button

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'basicbutton.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,15 @@ class HomeScreen extends StatelessWidget {
         title: Center(
           child: Text("AbSentry"),
         ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              BasicButton(),
+            ],
+          ),
+        ],
       ),
     );
   }
