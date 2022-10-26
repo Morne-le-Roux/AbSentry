35a32ffa47d1ba5b783cd696b2b4f70629b0b8b7
Morné le Roux <mornelerx@gmail.com>
2022-10-26 17:57:13 +0200

    Initial Commit
    Blank App

---
+++ android/app/src/main/kotlin/com/example/absentry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.absentry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AbSentry());
+}
+
+class AbSentry extends StatelessWidget {
+  const AbSentry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
