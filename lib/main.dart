faa80e422c610048a6aec4654fa466a0f491e40a
Morné le Roux <mornelerx@gmail.com>
2022-11-02 17:01:29 +0200

    Added Firebase Backend

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,12 @@ import 'package:absentry/newclassscreen.dart';
 import 'package:absentry/newentryscreen.dart';
 import 'package:absentry/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const AbSentry());
 }
 
