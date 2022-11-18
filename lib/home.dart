64ad7ec89c5fdf078aeb9351abd2a6b04859f09a
Morné le Roux <mornelerx@gmail.com>
2022-11-18 20:27:43 +0200

    Revert "Revert "removed unused import""
    This reverts commit 7dd439cf588571f7c8af34adeb601295b8172864.

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'basicbutton.dart';
-import 'package:absentry/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
