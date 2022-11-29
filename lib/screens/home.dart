3db5f7e5b2e3ec911c93cb7972697ef785b801ba
Morné le Roux <mornelerx@gmail.com>
2022-11-29 16:45:33 +0200

    removed unused code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,11 +12,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: DecoratedBox(
