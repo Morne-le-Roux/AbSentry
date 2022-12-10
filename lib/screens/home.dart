cde3a19394fce16f7739053b5e146a5721fcbba9
Morné le Roux <mornelerx@gmail.com>
2022-12-10 18:11:43 +0200

    added report button to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,52 +19,70 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Column(
-        children: [
-          SizedBox(height: 30),
+      body: SafeArea(
+        child: Column(
+          //
+          // HOME SCREEN SPACING (I CHANGE THIS A LOT CUZ I CANT MAKE UP MY MIND
+          // THIS IS BASICALLY JUST A MARKER.)
+          //
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-          //App's Name in the middle of the screen.
+          children: [
+            //App's Name in the middle of the screen.
 
-          Center(
-            child: Hero(
-              tag: "splashToLogin",
-              child: Text(
-                "AbSentry",
-                style:
-                    GoogleFonts.bebasNeue(color: kButtonColor, fontSize: 90.0),
+            Center(
+              child: Hero(
+                tag: "splashToLogin",
+                child: Text(
+                  "AbSentry",
+                  style: GoogleFonts.bebasNeue(
+                      color: kButtonColor, fontSize: 90.0),
+                ),
               ),
             ),
-          ),
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //
-                //Button that goes to the NewClassScreen. Allows the user to add a new class.
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //
+                  //Button that goes to the NewClassScreen. Allows the user to add a new class.
 
-                BasicButton(
-                  icon: Icon(Icons.group_add),
-                  text: "New Class",
-                  onpressed: () {
-                    Navigator.pushNamed(context, "/newClassScreen");
-                  },
-                ),
+                  BasicButton(
+                    icon: Icon(Icons.group_add),
+                    text: "New Class",
+                    onpressed: () {
+                      Navigator.pushNamed(context, "/newClassScreen");
+                    },
+                  ),
 
-                //
-                //Button that goes to the ClassSelectionScreen.
-                //Allows the user to input today's entry.
+                  //
+                  //Button that goes to the ClassSelectionScreen.
+                  //Allows the user to input today's entry.
 
-                BasicButton(
-                  icon: Icon(Icons.list_alt),
-                  text: "New Entry",
-                  onpressed: () {
-                    Navigator.pushNamed(context, "/classSelection");
-                  },
-                ),
-              ],
+                  BasicButton(
+                    icon: Icon(Icons.list_alt),
+                    text: "New Entry",
+                    onpressed: () {
+                      Navigator.pushNamed(context, "/classSelection");
+                    },
+                  ),
+
+                  //
+                  //Button to that goes to the report screen
+                  //
+
+                  BasicButton(
+                      icon: Icon(Icons.file_download),
+                      text: "Report (TBA)",
+                      onpressed: () {
+                        Navigator.pushNamed(
+                            context, "routeName"); //TODO: Implement route name
+                      })
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
